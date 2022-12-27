2124de977ced3018beac72670b6fbd0a4c9c5a30
biscuitboost <107883057+biscuitboost@users.noreply.github.com>
2022-12-27 23:49:11 +0000

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -57,11 +57,39 @@ const callGenerateEndpoint = async () => {
   <div className="prompt-buttons">
     <a className="generate-button" onClick={callGenerateEndpoint}>
       <div className="generate">
-        <p>Ask</p>
+        <p>Ask A/i</p>
       </div>
     </a>
   </div>
-          
+  <div className="prompt-container">
+  <textarea
+    placeholder="start typing here"
+    className="prompt-box"
+    value={userInput}
+    onChange={onUserChangedText}
+  />
+<div className="prompt-buttons">
+  <a className={isGenerating ? 'generate-button loading' : 'generate-button'}
+    onClick={callGenerateEndpoint}>
+    <div className="generate">
+    {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+    </div>
+  </a>
+</div>
+  {/* New code I added here */}
+  {apiOutput && (
+  <div className="output">
+    <div className="output-header-container">
+      <div className="output-header">
+        <h3>Output</h3>
+      </div>
+    </div>
+    <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+)}
+</div>
         </div>
       </div>
       <div className="badge-container grow">
