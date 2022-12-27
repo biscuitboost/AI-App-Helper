2a3f570cee349999c810f1a82a131d9d47e20efa
biscuitboost <107883057+biscuitboost@users.noreply.github.com>
2022-12-27 22:44:47 +0000

    Added generate button and new headline

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,45 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
+
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (event) => {
+    {/*console.log(event.target.value); */}
+  setUserInput(event.target.value);
+};
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>SmartSQL AI | robertbrewer.co.uk</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>SmartSQL AI</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Get Answers to Your SQL Queries Instantly with SmartSQL AI</h2>
           </div>
+          {/* Add this code here*/}
+        <div className="prompt-container">
+        <textarea
+          className="prompt-box"
+          placeholder="how do I get the current date?"
+          value={userInput}
+          onChange={onUserChangedText}/>       
+        </div>
+           {/* New code I added here */}
+  <div className="prompt-buttons">
+    <a className="generate-button" onClick={null}>
+      <div className="generate">
+        <p>Ask</p>
+      </div>
+    </a>
+  </div>
+          
         </div>
       </div>
       <div className="badge-container grow">
