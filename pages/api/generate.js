362b9cae00d328cb01a5f0cc9709304bd6b21c2e
biscuitboost <107883057+biscuitboost@users.noreply.github.com>
2023-03-06 16:22:49 +0000

    upgraded to 3.5 turbo!

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,20 +6,33 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 const basePromptPrefix =
-  "You are Robert, a knowlegable local guide who lives and works in Glasgow. Respond to questions with suggestions of places to visit and things to do in Glasgow. You speak great english but sometimes use some scots words. If the question is unrelated to Glasgow or being a travel guide respond to politely let them know you are just a Glasgow travel guide. My first question about Glasgow is ";
+  "You are named Robert, a knowlegable local guide who lives and works in Glasgow. Respond to questions with suggestions of places to visit and things to do in Glasgow. You speak great english but sometimes use some scots words. If the question is unrelated to Glasgow or being a travel guide respond to politely let them know you are just a Glasgow travel guide. My first question about Glasgow is ";
 const generateAction = async (req, res) => {
   // Run first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
 
+  /*
   const baseCompletion = await openai.createCompletion({
-    //model: "text-davinci-003",
-    model: "gpt-3.5-turbo",
-    prompt: `${basePromptPrefix}${req.body.userInput}` + "\n",
+    model: "text-davinci-003",
+    reminders: [{ name: "User", tasks: messages }],
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
     temperature: 0.3,
     max_tokens: 256,
   });
+*/
+  const turboCompletion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: "system", content: "${basePromptPrefix}" },
+      { role: "user", content: '"${req.body.userInput}"' },
+    ],
+    temperature: 0.3,
+    max_tokens: 256,
+    top_p: 1,
+  });
 
-  const basePromptOutput = baseCompletion.data.choices.pop();
+  //const basePromptOutput = baseCompletion.data.choices.pop();
+  const basePromptOutput = turboCompletion.data.choices.pop();
 
   res.status(200).json({ output: basePromptOutput });
 };
