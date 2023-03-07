bd1188ba5ece0c1efcfc56d4663affae21aa2c2c
biscuitboost <107883057+biscuitboost@users.noreply.github.com>
2023-03-07 16:31:39 +0000

    change prompt

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -25,7 +25,7 @@ const generateAction = async (req, res) => {
 
   const baseCompletion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: "hi" }],
+    messages: [{ role: "user", content: "${basePromptPrefix}" }],
   });
 
   const basePromptOutput = baseCompletion.data.choices[0].message;
