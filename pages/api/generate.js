20da7499e3dfbcaad232971c79c7657d37add9c2
biscuitboost <107883057+biscuitboost@users.noreply.github.com>
2023-03-07 09:53:21 +0000

    testing

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -7,34 +7,43 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 const basePromptPrefix =
   "You are named Robert, a knowlegable local guide who lives and works in Glasgow. Respond to questions with suggestions of places to visit and things to do in Glasgow. You speak great english but sometimes use some scots words. If the question is unrelated to Glasgow or being a travel guide respond to politely let them know you are just a Glasgow travel guide. My first question about Glasgow is ";
-const generateAction = async (req, res) => {
-  // Run first prompt
-  //console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
+try {
+  const generateAction = async (req, res) => {
+    // Run first prompt
+    //console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
 
-  /*
-  const baseCompletion = await openai.createCompletion({
-    model: "text-davinci-003",
-    reminders: [{ name: "User", tasks: messages }],
-    prompt: `${basePromptPrefix}${req.body.userInput}`,
-    temperature: 0.3,
-    max_tokens: 256,
-  });
-*/
+    /*
+    const baseCompletion = await openai.createCompletion({
+      model: "text-davinci-003",
+      reminders: [{ name: "User", tasks: messages }],
+      prompt: `${basePromptPrefix}${req.body.userInput}`,
+      temperature: 0.3,
+      max_tokens: 256,
+    });
+  */
 
-  const turboCompletion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: "hi" }],
-  });
-  console.log(turboCompletion.data.choices[0].message.content);
+    const turboCompletion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: "hi" }],
+    });
+    console.log(turboCompletion.data.choices[0].message.content);
 
-  //output turboCompletion as json to console
+    //output turboCompletion as json to console
 
-  //const basePromptOutput = baseCompletion.data.choices.pop();
-  //console.log(turboCompletion);
-  //const basePromptOutput = turboCompletion.data.choices.pop.message();
+    //const basePromptOutput = baseCompletion.data.choices.pop();
+    //console.log(turboCompletion);
+    //const basePromptOutput = turboCompletion.data.choices.pop.message();
 
-  //res.status(200).json({ output: basePromptOutput });
-  //res.status(200).json({ content: basePromptOutput });
-};
+    //res.status(200).json({ output: basePromptOutput });
+    //res.status(200).json({ content: basePromptOutput });
+  };
+} catch (error) {
+  if (error.response) {
+    console.log(error.response.data);
+    console.log(error.response.status);
+  } else {
+    console.log(error);
+  }
+}
 
 export default generateAction;
