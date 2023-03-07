62b6134ebe2a4aa2d76824bff30c0e2d8c51fd4d
biscuitboost <107883057+biscuitboost@users.noreply.github.com>
2023-03-07 16:14:25 +0000

    test

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -28,7 +28,7 @@ const generateAction = async (req, res) => {
     messages: [{ role: "user", content: "hi" }],
   });
 
-  const basePromptOutput = baseCompletion.data.choices[0].message.pop();
+  const basePromptOutput = baseCompletion.data.choices[0].message;
 
   res.status(200).json({ output: basePromptOutput });
 };
